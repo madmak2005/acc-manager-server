b8040a6f0bd5c43ddcfbebdee5da74662bacac71
Tomek <tomasz.makowski@fastdata.com.pl>
2021-03-04 16:40:39 +0100

    save collected sessions: [GET] http://localhost:8080/save

--- src/main/java/ACC/rest/RestControler.java
+++ src/main/java/ACC/rest/RestControler.java
@@ -1,9 +1,14 @@
 package ACC.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ACC.model.OutputMessage;
+import ACC.model.PageFileStatistics;
 import ACC.sharedmemory.ACCSharedMemoryService;
 
 @RestController
@@ -26,4 +31,15 @@ public class RestControler {
 	public String getGraphicsJson() {
 		return accSharedMemoryService.getPageFile("graphics").toJSON();
 	}
+	
+	@GetMapping("/save")
+	public String getSaveSessions() {
+		List<String> fieldsStatistics = new ArrayList<String>();
+		OutputMessage om = accSharedMemoryService.getPageFileMessage("statistics", fieldsStatistics);
+		PageFileStatistics statistics = (PageFileStatistics) om.page;
+		statistics.saveToXLSX();
+		return statistics.toJSON();
+	}
+	
+
 }
