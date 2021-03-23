58e64defec8d1376fa8b169b23c3140405b14bc6
madmak <madmak2005@gmail.com>
2021-03-23 16:38:04 +0100

    google api fight part 2

---
+++ src/main/java/ACC/saving/SecurityConfig.java
@@ -0,0 +1,21 @@
+package ACC.saving;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/", "/login.html").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login().permitAll()
+                .and().
+                logout().logoutSuccessUrl("/");
+        
+    }
+}
